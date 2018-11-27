7ce4fb1fdd8c059ad1dc5183d85c7985b5cf6db7
Jeff Oliver <jeffoliver3384@gmail.com>
2018-11-27 18:42:08 -0500

    Diff Two Arrays

---
+++ Sum All Numbers in a Range/app.js
@@ -0,0 +1,13 @@
+function sumAll(arr) {
+  let newNumArray = arr.sort((a,b) => a-b),
+        num1 = newNumArray[0],
+        num2 = newNumArray[1];
+
+  while(num2 > num1+1) {
+    num2--;
+    newNumArray.push(num2);
+  };
+  return newNumArray.reduce((total, num) => total + num);
+}
+
+sumAll([1, 4]);

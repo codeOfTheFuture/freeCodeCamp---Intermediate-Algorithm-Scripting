d83e4710f271c8a8850ba611006a0a6d752cf10b
Jeff Oliver <jeffoliver3384@gmail.com>
2018-12-12 20:09:53 -0500

    Convert HTML Entities

---
+++ Convert HTML Entities/app.js
@@ -0,0 +1,26 @@
+function convertHTML(str) {
+  let strArray = str.split('');
+
+  for(let i = 0; i < strArray.length; i++) {
+    switch (strArray[i]) {
+      case '&':
+        strArray[i] = '&amp;';
+        break;
+      case '<':
+        strArray[i] = '&lt;';
+        break;
+      case '>':
+        strArray[i] = '&gt';
+        break;
+      case '"':
+        strArray[i] = '&quot;';
+        break;
+      case "'":
+        strArray[i] = '&apos;';
+        break;
+    }
+  }
+  return strArray.join('');
+}
+
+convertHTML("Dolce & Gabbana");

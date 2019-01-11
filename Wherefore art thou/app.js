4bc57ae0d53bda762d612ab5c9d06056187ea201
Jeff Oliver <jeffoliver3384@gmail.com>
2019-01-10 23:11:23 -0500

    Wherefore art thou

--- Wherefore art thou/app.js
+++ Wherefore art thou/app.js
@@ -1,15 +1,13 @@
 function whatIsInAName(collection, source) {
-  // What's in a name?
-  var arr = [];
-  // Only change code below this line
-  
-  console.log(arr)
-  // Only change code above this line
-  return arr;
+  const array = collection.filter(obj => {
+    for(let i in source) {
+      if(source[i] !== obj[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  return array;
 }
 
-// whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
-
-// whatIsInAName([{ "apple": 1 }, { "apple": 1 }, { "apple": 1, "bat": 2 }], { "apple": 1 });
-
-whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
